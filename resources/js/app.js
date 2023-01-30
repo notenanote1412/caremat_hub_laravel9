da231b4f087a6451aafd24f8516be8ca7b72b69d
Kiattisak Puanta <Kiattisak.Puanta1412@gmail.com>
2023-01-30 09:43:05 +0700

    add tailwind css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+
+import app from './component/app.vue'
+
+createApp(app).mount("#app")
---
+++ resources/js/component/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-3xl text-red-500">
+        Laravel 9 vite with vue 3
+    </h1>
+</template>
